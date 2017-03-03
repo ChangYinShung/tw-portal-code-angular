d44217cd8ffc1c98747243b5d0ac146677db3733
ChangYinShung <changyenshung@gmail.com>
2017-03-03 19:37:44 +0800

    TEST

---
+++ src/SamplePorject/gulpfile.js
@@ -0,0 +1,21 @@
+/// <binding AfterBuild='default' />
+var gulp = require("gulp");
+var ngAnnotate = require('gulp-ng-annotate');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var paths = {
+  'dev': ['./wwwroot/dev/**.js']
+};
+var gulpCopy = require('gulp-copy');
+
+
+gulp.task('default', ['uncompress', 'compress']);
+
+gulp.task('compress', function () {
+  return gulp.src(paths.dev).pipe(ngAnnotate()).pipe(uglify()).pipe(concat('twProtalCode.directive.min.js')).pipe(gulp.dest('./wwwroot/dist')).pipe(gulpCopy('../../dist', { prefix: 3 }));
+});
+gulp.task('uncompress', function () {
+  return gulp.src(paths.dev).pipe(ngAnnotate()).pipe(concat('twProtalCode.directive.js')).pipe(gulp.dest('./wwwroot/dist')).pipe(gulpCopy('../../dist', { prefix: 3 }));
+});
+
+
